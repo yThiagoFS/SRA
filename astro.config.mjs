64a337da882804d44ac669214a57a9e9c109d589
Thiago Silva <thi.ferreira.silva03@gmail.com>
2026-08-24 21:30:16 -0300

    fix: assets file

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://ythiagofs.github.io',
+	base: '/SRA',
 	markdown: {
 		processor: unified({
 			remarkPlugins: [remarkMath],
